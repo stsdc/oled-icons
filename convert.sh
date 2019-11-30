8e7f355bedbf11831da64e7f775d37d44d6879f0
stsdc <stanislaw.dac@gmail.com>
2019-11-30 20:20:15 +0100

    add pgmspace header

--- convert.sh
+++ convert.sh
@@ -3,31 +3,34 @@
 echo "┌───────────────────────────────────────┐"
 echo "│ Converting oled-icons from SVG to C++ │"
 echo "└───────────────────────────────────────┘"
-echo -n " * Creating xbm directory ...         "
+echo -n " * Creating xbm directory ........... "
 mkdir -p bitmaps
 echo "ok"
 
-echo -n " * Changing directory to svg ...      "
+echo -n " * Changing directory to svg ........ "
 cd svg
 echo "ok"
 
-echo -n " * Converting to bitmaps ...          "
+echo -n " * Converting to bitmaps ............ "
 mogrify -path ../bitmaps -format xbm  *.svg
 echo "ok"
 
-echo -n " * Changing directory to bitmaps ...  "
+echo -n " * Changing directory to bitmaps .... "
 cd ../bitmaps
 echo "ok"
 
-echo -n " * Changing file extensions ...       "
+echo -n " * Changing file extensions ......... "
 for f in *.xbm; do 
     mv -- "$f" "icon_${f%.*}.cpp"
 done
 echo "ok"
 
-echo -n " * Replacing variable names ...       "
+echo -n " * Patching ......................... "
 for f in *.cpp; do
-    replace="const unsigned char ${f%.*} [] PROGMEM = {"
-    sed -i "3s/.*/$replace/" $f
+    prepend="#include <avr/pgmspace.h>\n\n"
+    sed -i "1s;^;$prepend;" $f
+
+    replace="\nconst unsigned char ${f%.*} [] PROGMEM = {"
+    sed -i "5s/.*/$replace/" $f
 done
 echo "ok"

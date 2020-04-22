3df247a84cf9dee25e205313c371695d8a6716a5
Stanisław <stanislaw.dac@gmail.com>
2020-04-22 22:11:31 +0200

    remove PROGMEM to make work with esp32

--- src/bitmaps.h
+++ src/bitmaps.h
@@ -1,23 +1,21 @@
-#include <avr/pgmspace.h>
-
-extern const unsigned char icon_elementary_logo [] PROGMEM;
+extern const unsigned char icon_elementary_logo [];
 #define  icon_elementary_logo_width 65
 #define  icon_elementary_logo_height 65
-extern const unsigned char icon_serial_connected [] PROGMEM;
+extern const unsigned char icon_serial_connected [];
 #define  icon_serial_connected_width 16
 #define  icon_serial_connected_height 16
-extern const unsigned char icon_serial_disconnected [] PROGMEM;
+extern const unsigned char icon_serial_disconnected [];
 #define  icon_serial_disconnected_width 16
 #define  icon_serial_disconnected_height 16
-extern const unsigned char icon_wifi4ap [] PROGMEM;
+extern const unsigned char icon_wifi4ap [];
 #define  icon_wifi4ap_width 16
 #define  icon_wifi4ap_height 16
-extern const unsigned char icon_wifi4connecting [] PROGMEM;
+extern const unsigned char icon_wifi4connecting [];
 #define  icon_wifi4connecting_width 16
 #define  icon_wifi4connecting_height 16
-extern const unsigned char icon_wifi4 [] PROGMEM;
+extern const unsigned char icon_wifi4 [];
 #define  icon_wifi4_width 16
 #define  icon_wifi4_height 16
-extern const unsigned char icon_wifi_disconnected [] PROGMEM;
+extern const unsigned char icon_wifi_disconnected [];
 #define  icon_wifi_disconnected_width 16
 #define  icon_wifi_disconnected_height 16
